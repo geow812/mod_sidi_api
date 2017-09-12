232c99ab00325a976f313db8c0c0adbd39c1dd16
root <bsknight58@gmail.com>
2017-09-12 10:34:19 +0800

    commit

--- mod.py
+++ mod.py
@@ -83,7 +83,7 @@ def sidi_clear_position(context, bar_dict, account_id):
             logger.warn(e)
             continue
 #count_type: 0  use context.buy_stock_count  1 use len(buy_stocks)
-def sidi_adjust_position(context, bar_dict, buy_stocks, account_id, count_type=0):
+def sidi_adjust_position(context, bar_dict, buy_stocks, account_id, count_type=0, position_per=1):
     res_json =  sidi_get_position(account_id)
     if res_json.has_key('results')==False:
         logger.warn("error position type %s" % res_json)
@@ -117,14 +117,14 @@ def sidi_adjust_position(context, bar_dict, buy_stocks, account_id, count_type=0
                 continue
 
     # waiting for trading done
-    time.sleep(3)
+    time.sleep(10)
 
     position_count = len(position_data) - num
     #logger.warn(len(buy_stocks))
     if len(buy_stocks) == 0:
         return
     if context.buy_stock_count > position_count:
-        cash = float(sidi_get_cash(account_id)['results'][0]['userable_balance'])
+        cash = float(sidi_get_cash(account_id)['results'][0]['userable_balance']) * float(position_per)
         logger.warn("total cash %f" % cash)
         if count_type == 1:
             value =  cash / len(buy_stocks)
